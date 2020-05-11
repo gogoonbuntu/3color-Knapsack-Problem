da5bc70073355c7a5477b35da0e7d86b4afb5bd2
Gogoonbuntu <tmddud333@naver.com>
2020-05-11 18:07:51 +0000

    ㄴㅇㄹ

--- main.c
+++ main.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
+//struct 선언이 꼭 필요한가?
+
+int main(void){
+	
+	
+}
+
+
+int randomGenerate(int num){
+	
+	for(int i=0; i<num; i++){
+		rand();
+	}
+	return 0;
+}
