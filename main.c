7c1407c7833ec9ef29c460c07f37b89df4217b8e
Gogoonbuntu <tmddud333@naver.com>
2020-05-12 17:24:11 +0000

    random generate complete

--- main.c
+++ main.c
@@ -2,18 +2,36 @@
 #include <stdlib.h>
 #include <time.h>
 
-//struct 선언이 꼭 필요한가?
+int weight[10000] = {0, };
+int benefit[10000] = {0, };
+
+int randomGenerate(int num);
+void printArray(int a[]);
 
 int main(void){
-	
-	
+	randomGenerate(10);
+	printArray(weight);
+	printArray(benefit);
 }
 
 
 int randomGenerate(int num){
+
+	srand((unsigned int)time(NULL));
 	
 	for(int i=0; i<num; i++){
-		rand();
+		weight[i] = (rand()+1)%100;
+		benefit[i] = (rand()+1)%300;
 	}
+	
 	return 0;
+}
+
+void printArray(int a[]) {
+	int i=0;
+	while(a[i]!=0){
+		printf("%d: %d\n", i, a[i]);
+		i++;
+	}
+	//printf("\n");
 }
