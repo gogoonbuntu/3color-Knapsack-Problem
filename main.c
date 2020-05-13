9c8f2e5465f91951ab9cc4e9e66daa259a8d8850
Gogoonbuntu <tmddud333@naver.com>
2020-05-13 12:04:41 +0000

    DP done

--- main.c
+++ main.c
@@ -4,17 +4,19 @@
 
 int weight[10000] = {0, };
 int benefit[10000] = {0, };
-int W = 0;
+int Capacity = 0;
+char answer_DP[100];
 
 int randomGenerate(int itemNum);
 void printArray(int a[]);
-char* DP(int w, int itemNum);
+char* DP(int itemNum);
 
 int main(void){
 	randomGenerate(10);
-	DP(10, 10);
-	//printArray(weight);
-	//printArray(benefit);
+	printArray(weight);
+	printArray(benefit);
+	DP(10);
+	
 }
 
 
@@ -27,46 +29,56 @@ int randomGenerate(int itemNum){
 		benefit[i] = rand()%299+1;
 	}
 	
-	W = itemNum*40;
+	Capacity = itemNum*40;
 	printf("random generated\n");
 	return 0;
 }
 
-char* DP(int w, int itemNum) {
+char* DP(int itemNum) {
+	clock_t start_DP = clock();
+
 	//동적배열 생성
 	int** B = malloc(sizeof(int*) * itemNum);
-	for(int i=0; i<itemNum; i++){
-		B[i] = malloc(sizeof(int) * w);
+	for(int i=0; i<=itemNum; i++){
+		B[i] = malloc(sizeof(int) * Capacity);
 		//B[item][weight]
 	}
 
 	printf("B array generated\n");
 
-	for(int j=0; j<w; j++){
+	for(int j=0; j<=Capacity; j++){
 		B[0][j] = 0;
 	}
-	for(int i=1; i<itemNum; i++){
+	for(int i=1; i<=itemNum; i++){
 		B[i][0] = 0;
-		for(int j=1; j<w; j++){
-			if(weight[i]<=W){
+		for(int j=1; j<=Capacity; j++){
+			if(weight[i]<=j){
 				int option1 = benefit[i]+B[i-1][j-weight[i]];
 				int option2 = B[i-1][j];
-				printf("[%d][%d] op1 : %d, op2: %d\n", i, j, option1, option2);
 				B[i][j]= option1 > option2 ? option1 : option2;
-				//printf("B[%d][%d] : %d\n", i, j, B[i][j]);
 			} else {
 				B[i][j]= B[i-1][j];
 			}
+			//printf("B[%d][%d] : %d\n", i, j, B[i][j]);
 		}
 	}
-	return 0;
+	clock_t end_DP = clock();
+
+	float time_DP = end_DP - start_DP;
+
+	answer_DP[0] = '\0';
+	sprintf(answer_DP, "%d / %.3f", B[10][400], time_DP);
+	printf("answer_DP = %s\n", answer_DP);
+	return answer_DP;
 }
 
 
 void printArray(int a[]) {
-	int i=0;
-	while(a[i]!=0){
-		printf("%d: %d\n", i, a[i]);
+	printf("print start\n");
+	int i=1;
+	while(a[i]>0){
+		printf("%d: %-5d", i, a[i]);
 		i++;
+		i%5==1 ? printf("\n") : 0;
 	}
 }
